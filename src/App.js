8b5145b17aa20b3c6e35cc40784b865a2b14b224
unknown <andre.sullivan18@hotmail.com>
2022-05-14 01:27:46 -0300

    pequena alteração

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Aprendendo a usar o React e o java script dhsauuhdsahdau
+          Aprendendo a usar o React
           
         </a>
       </header>
