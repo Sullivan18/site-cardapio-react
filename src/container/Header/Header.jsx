8846a5ce7ddf9eb94c854380e185b4e42e74d1ac
unknown <andre.sullivan18@hotmail.com>
2022-05-15 20:32:22 -0300

    primeiro commit

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -7,8 +7,8 @@ const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
       <SubHeading title='Chase the new flavour'/>
-      <h1 className='app__header-h1'>A CHAVE PARA UM REQUINTADO JANTAR</h1>
-      <p className="p__opensans" style={{margin: '2rem 0'}}></p>
+      <h1 className='app__header-h1'>A CHAVE PARA UM JANTAR REQUINTADO</h1>
+      <p className="p__opensans" style={{margin: '2rem 0'}}>O sullivão dá o botão enquanto joga o mirão</p>
     </div>
     
     <div className="app__wrapper_img">
