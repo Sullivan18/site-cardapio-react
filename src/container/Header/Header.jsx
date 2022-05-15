f57c4566c9a939282136b6990e01ba784c146b89
unknown <andre.sullivan18@hotmail.com>
2022-05-15 20:49:46 -0300

    primeiro commit

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -8,11 +8,13 @@ const Header = () => (
     <div className="app__wrapper_info">
       <SubHeading title='Chase the new flavour'/>
       <h1 className='app__header-h1'>A CHAVE PARA UM JANTAR REQUINTADO</h1>
-      <p className="p__opensans" style={{margin: '2rem 0'}}>O sullivão dá o botão enquanto joga o mirão</p>
+      <p className="p__opensans" style={{ margin: '2rem 0'}}>faaaaaadfffffffffffffffffffffffffffffffffffffffffffffffff fffffffffffffffffff</p>
+      <button type='button' className="custom__button">Explore Menu</button>
+
     </div>
     
     <div className="app__wrapper_img">
-
+      <img src={images.welcome} alt="header img" />
 
     </div>
   </div>
